ff2d0a89f69ed853082bdd696f34af87f2f59219
Mapa <julian147_@Outlook.es>
2025-09-23 20:06:42 -0500

    algo de mejora
    utilice pywal para que la hora de cambiar fondo de pantalla el tema del fondo se transfiera a kitty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,13 @@ if status is-interactive && type -q fastfetch
     fastfetch
     # Commands to run in interactive sessions can go here
 end
+
+
+
+# Aplicar pywal sequences al iniciar cada shell interactiva
+if test -f $HOME/.cache/wal/sequences
+    if test -w /dev/tty
+        cat $HOME/.cache/wal/sequences > /dev/tty 2>/dev/null
+    end
+end
+
